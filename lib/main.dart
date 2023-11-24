efde3d438b996cb9b3fd62b6ac1e8ae6c417267a
annorax <ido.dovrat@gmail.com>
2023-11-24 16:35:33 +0200

    Remove unneeded code

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 import 'package:get/get.dart';
 
 import 'package:slim_travel_frontend/services/auth_service.dart';
@@ -7,12 +6,6 @@ import 'package:slim_travel_frontend/services/auth_service.dart';
 import 'app/modules/home/view.dart';
 
 void main() {
-  SystemChrome.setSystemUIOverlayStyle(
-    SystemUiOverlayStyle(
-      systemNavigationBarColor:
-          SystemUiOverlayStyle.dark.systemNavigationBarColor,
-    ),
-  );
   Get.put(AuthService());
   runApp(
     const GetMaterialApp(

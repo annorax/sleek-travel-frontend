3c1553e839148315f9306889bc35c77596cef9f8
annorax <ido.dovrat@gmail.com>
2023-11-28 12:31:21 +0200

    Revert to initial commit (generated project)

--- lib/app/modules/login/bindings.dart
+++
@@ -1,9 +0,0 @@
-import 'package:get/get.dart';
-import 'package:slim_travel_frontend/app/modules/login/controller.dart';
-
-class LoginBindings implements Bindings {
-  @override
-  void dependencies() {
-    Get.lazyPut<LoginController>(() => LoginController());
-  }
-}
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,12 +8,12 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import '../lib/app/modules/home/view.dart';
+import 'package:slim_travel_frontend/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const Home());
+    await tester.pumpWidget(const MyApp());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);

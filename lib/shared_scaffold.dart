56de79b8de2a796733ca8fabb2486b0c84cd91a0
annorax <ido.dovrat@gmail.com>
2024-03-10 15:36:17 +0200

    Add account button

--- lib/shared_scaffold.dart
+++ lib/shared_scaffold.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:slim_travel_frontend/user.model.dart';
+import 'package:slim_travel_frontend/user.state.dart';
 
 class SharedScaffold extends StatelessWidget {
   final String title;
@@ -8,10 +10,29 @@ class SharedScaffold extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    User? user = userState.getValueSyncNoInit();
+    final GlobalKey<TooltipState> tooltipkey = GlobalKey<TooltipState>();
+
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         title: Text(title),
+        actions: [
+          Tooltip(
+            key: tooltipkey,
+            triggerMode: TooltipTriggerMode.manual,
+            //enableTapToDismiss: false,
+            preferBelow: true,
+            message:
+                user?.id != null ? "Logged in as ${user!.name}" : "Logged out",
+            child: IconButton(
+              icon: const Icon(Icons.account_circle),
+              onPressed: () {
+                  tooltipkey.currentState?.ensureTooltipVisible();
+              }
+            ),
+          ),
+        ],
       ),
       body: child,
     );

01325ccfdf93e7b534065a0dd64143c0070ca4f1
Ido Dovrat <ido.dovrat@gmail.com>
2025-06-04 15:38:03 +0300

    Refine login page appearance

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -36,92 +36,85 @@ class _LoginPageState extends State<LoginPage> {
           child: Form(
             key: _formKey,
             autovalidateMode: AutovalidateMode.onUnfocus,
-            child: Column(
-              children: [
-                SizedBox(height: 16),
-                TextFormField(
-                  key: emailOrPhoneFieldKey,
-                  decoration: const InputDecoration(labelText: 'Email / phone number'),
-                  validator: ValidationBuilder().or(
-                    (builder) => builder.email(),
-                    (builder) => builder.regExp(RegExp(r'\+\d+'), 'Invalid email address / phone number'),
-                  ).required('Required').build()
-                ),
-                const SizedBox(height: 16),
-                TextFormField(
-                  key: passwordFieldKey,
-                  decoration: const InputDecoration(labelText: 'Password'),
-                  obscureText: true,
-                  validator: (value) => (value == null || value.isEmpty) ? 'Required' : null
-                ),
-                Padding(
-                  padding: const EdgeInsets.only(top: 10),
-                  child: MaterialButton(
-                    color: Theme.of(context).colorScheme.secondary,
-                    onPressed: () async {
-                      if (!_formKey.currentState!.validate()) {
-                        return;
-                      }
-                      final OperationResponse result = await client.request(
-                        GLogInUserReq(
-                          (builder) =>
-                            builder.vars
-                              ..emailOrPhone = extractValue(emailOrPhoneFieldKey)
-                              ..password = extractValue(passwordFieldKey)
-                        )
-                      ).firstWhere((response) => response.dataSource != DataSource.Optimistic);
-                      if (result.hasErrors) {
-                        print(result.graphqlErrors);
-                        if (context.mounted) {
-                          showError("Login failed", context);
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Column(
+                children: [
+                  TextFormField(
+                    key: emailOrPhoneFieldKey,
+                    decoration: const InputDecoration(labelText: 'Email / phone number'),
+                    validator: ValidationBuilder().or(
+                      (builder) => builder.email(),
+                      (builder) => builder.regExp(RegExp(r'\+\d+'), 'Invalid email address / phone number'),
+                    ).required('Required').build()
+                  ),
+                  const SizedBox(height: 16),
+                  TextFormField(
+                    key: passwordFieldKey,
+                    decoration: const InputDecoration(labelText: 'Password'),
+                    obscureText: true,
+                    validator: (value) => (value == null || value.isEmpty) ? 'Required' : null
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(top: 10),
+                    child: MaterialButton(
+                      color: Theme.of(context).colorScheme.secondary,
+                      onPressed: () async {
+                        if (!_formKey.currentState!.validate()) {
+                          return;
                         }
-                      }
-                      dynamic logInUser = result.data.logInUser;
-                      if (logInUser == null) {
-                        if (context.mounted) {
-                          showError("Login failed", context);
+                        final OperationResponse result = await client.request(
+                          GLogInUserReq(
+                            (builder) =>
+                              builder.vars
+                                ..emailOrPhone = extractValue(emailOrPhoneFieldKey)
+                                ..password = extractValue(passwordFieldKey)
+                          )
+                        ).firstWhere((response) => response.dataSource != DataSource.Optimistic);
+                        if (result.hasErrors) {
+                          print(result.graphqlErrors);
+                          if (context.mounted) {
+                            showError("Login failed", context);
+                          }
                         }
-                        return;
-                      }
-                      final token = logInUser.token;
-                      final dynamic safeUser = logInUser.user;
-                      final User user = User(
-                        id: safeUser.id,
-                        name: safeUser.name,
-                        email: extractValue(emailOrPhoneFieldKey),
-                        token: token
-                      );
-                      await userState.setValue(user);
-                      NavigationManager.instance.pushReplacement(DashboardTab.items.name);
-                    },
-                    child: const Text('Login'),
-                  )
-                ),
-                Row(
-                  children: [
-                    Expanded(
-                      child: Divider(
-                        //thickness: 0.5,
-                        //color: Colors.grey[400],
+                        dynamic logInUser = result.data.logInUser;
+                        if (logInUser == null) {
+                          if (context.mounted) {
+                            showError("Login failed", context);
+                          }
+                          return;
+                        }
+                        final token = logInUser.token;
+                        final dynamic safeUser = logInUser.user;
+                        final User user = User(
+                          id: safeUser.id,
+                          name: safeUser.name,
+                          email: extractValue(emailOrPhoneFieldKey),
+                          token: token
+                        );
+                        await userState.setValue(user);
+                        NavigationManager.instance.pushReplacement(DashboardTab.items.name);
+                      },
+                      child: const Text('Login'),
+                    )
+                  ),
+                  const SizedBox(height: 16),
+                  Row(
+                    children: [
+                      Expanded(
+                        child: Divider(),
                       ),
-                    ),
-                    const Padding(
-                      padding: EdgeInsets.symmetric(
-                          horizontal: 10.0),
-                      child: Text(
-                        'Or',
-                        //style: TextStyle(color: Colors.white, fontSize: 16),
+                      const Padding(
+                        padding: EdgeInsets.symmetric(horizontal: 10.0),
+                        child: Text('Or'),
                       ),
-                    ),
-                    Expanded(
-                      child: Divider(
-                        //thickness: 0.5,
-                        //color: Colors.grey[400],
+                      Expanded(
+                        child: Divider(),
                       ),
-                    ),
-                  ],
-                ),
-              ],
+                    ],
+                  ),
+                ],
+              ),
             ),
           ),
         ));

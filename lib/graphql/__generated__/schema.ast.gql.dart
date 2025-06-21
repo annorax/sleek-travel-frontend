b03ae20f668ff4ca39190f9f8c2ee1aa84261bdd
annorax <ido.dovrat@gmail.com>
2025-06-21 08:37:19 +0300

    Generate client code for the new endpoint

--- lib/graphql/__generated__/schema.ast.gql.dart
+++ lib/graphql/__generated__/schema.ast.gql.dart
@@ -9299,6 +9299,25 @@ const Mutation = _i1.ObjectTypeDefinitionNode(
       ),
     ),
     _i1.FieldDefinitionNode(
+      name: _i1.NameNode(value: 'resendPasswordResetLink'),
+      directives: [],
+      args: [
+        _i1.InputValueDefinitionNode(
+          name: _i1.NameNode(value: 'email'),
+          directives: [],
+          type: _i1.NamedTypeNode(
+            name: _i1.NameNode(value: 'String'),
+            isNonNull: true,
+          ),
+          defaultValue: null,
+        )
+      ],
+      type: _i1.NamedTypeNode(
+        name: _i1.NameNode(value: 'Void'),
+        isNonNull: false,
+      ),
+    ),
+    _i1.FieldDefinitionNode(
       name: _i1.NameNode(value: 'resendPhoneNumberVerificationRequest'),
       directives: [],
       args: [

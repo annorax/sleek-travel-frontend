564fcaac9309f8418ebdd6a4ff20595b20ff5845
Ido Dovrat <ido.dovrat@gmail.com>
2025-07-08 12:40:44 +0300

    Handle edge case of SMS dispatch failure

--- lib/pages/sign_up_page.dart
+++ lib/pages/sign_up_page.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:form_validator/form_validator.dart';
 import 'package:navigation_utils/navigation_utils.dart';
 import 'package:sleek_travel_frontend/forms/pinput_form.dart';
+import 'package:sleek_travel_frontend/graphql/__generated__/mutations.data.gql.dart';
 import 'package:sleek_travel_frontend/graphql/__generated__/mutations.req.gql.dart';
 import 'package:sleek_travel_frontend/main.dart';
 import 'package:sleek_travel_frontend/pages/login_page.dart';
@@ -97,36 +98,39 @@ class _SignUpPageState extends State<SignUpPage> {
                         showError("Sign up failed", context);
                       }
                     } else {
-                      print(result.data);
-                      print(result.data.runtimeType);
-                      //print("UserId is $userId");
+                      GRegisterUserData_registerUser response = result.data.registerUser;
                       if (context.mounted) {
-                        String otp = await showModalBottomSheet(
-                          isScrollControlled: true,
-                          shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(10.0),
-                          ),
-                          builder: (BuildContext context) => PinputForm(phoneNumber: phoneNumber),
-                          context: context,
-                          useSafeArea: true
-                        );
-                        final OperationResponse result = await client.request(
-                          GVerifyPhoneNumberReq(
-                            (builder) =>
-                              builder.vars
-                                //..userId = userId
-                                ..otp = otp
-                          )
-                        ).firstWhere((response) => response.dataSource != DataSource.Optimistic);
-                        if (result.hasErrors) {
-                          print(result.graphqlErrors);
-                          if (context.mounted) {
-                            showError("Phone number verification failed", context);
+                        if (response.error != null && response.error!.toLowerCase().contains('sms')) {
+                          showError(response.error!, context);
+                        } else {
+                          String otp = await showModalBottomSheet(
+                            isScrollControlled: true,
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(10.0),
+                            ),
+                            builder: (BuildContext context) => PinputForm(phoneNumber: phoneNumber),
+                            context: context,
+                            useSafeArea: true
+                          );
+                          final OperationResponse result = await client.request(
+                            GVerifyPhoneNumberReq(
+                              (builder) =>
+                                builder.vars
+                                  ..userId.value = response.userId!.value
+                                  ..otp = otp
+                            )
+                          ).firstWhere((response) => response.dataSource != DataSource.Optimistic);
+                          if (result.hasErrors) {
+                            print(result.graphqlErrors);
+                            if (context.mounted) {
+                              showError("Phone number verification failed", context);
+                            }
+                          } else {
+                            if (context.mounted) {
+                              showInfo("Sign up successful. Please check your email.", context);
+                            }
                           }
                         }
-                      }
-                      if (context.mounted) {
-                        showInfo("Sign up successful. Please check your email.", context);
                       }
                       NavigationManager.instance.pushReplacement(LoginPage.name);
                     }
